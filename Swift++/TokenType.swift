48687afabdf0bbe40a5add94a77be49ec23fb48c
Swapnanil Dhol <swapnanildhol@gmail.com>
2021-04-05 01:03:36 +0530

    Tokens: Defined TokenType and Tokenizer class

---
+++ Swift++/TokenType.swift
@@ -0,0 +1,19 @@
+//
+//  TokenType.swift
+//  Swift++
+//
+//  Created by Swapnanil Dhol on 4/5/21.
+//
+
+import Foundation
+
+enum TokenType: String {
+    case TT_INT = "TT_INT"
+    case TT_FLOAT = "FLOAT"
+    case TT_PLUS = "PLUS"
+    case TT_MINUS = "MINUS"
+    case TT_MUL = "MUL"
+    case TT_DIV = "DIV"
+    case TT_LPAREN = "LPAREN"
+    case TT_RPAREN = "RPAREN"
+}
